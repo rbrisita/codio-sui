b86cd5c67f3d89668048a61b532dfdc35006dd24
Robert Brisita <rbrisita@hotmail.com>
2021-03-08 21:34:20 -0500

    Adding safe uri to write to. Fixes #3.

--- vscode/src/filesystem/workspace.ts
+++ vscode/src/filesystem/workspace.ts
@@ -1,6 +1,7 @@
+import * as vscode from 'vscode';
 import { workspace, Uri } from 'vscode';
 import { showCodioNameInputBox, UI, MESSAGES } from '../user_interface/messages';
-import { join } from 'path';
+import { basename, join } from 'path';
 import { ensureDir } from './saveProjectFiles';
 import { existsSync } from 'fs';
 
@@ -10,17 +11,17 @@ const createWorkspaceCodiosFolder = async (workspaceUri: Uri) => {
   return codioWorkspaceFolder;
 };
 
-export const getWorkspaceUriAndCodioDestinationUri = async () => {
-  let workspaceUri = null;
-  let codioUri = null;
-  let getCodioName = null;
+export const getWorkspaceUriAndCodioDestinationUri = async (): Promise<Record<string, unknown>> => {
+  let workspaceUri: Uri = null;
+  let codioUri: Uri = null;
+  let getCodioName: () => Promise<string> = null;
 
   if (workspace.workspaceFolders) {
-    const name = await showCodioNameInputBox();
+    let name = await showCodioNameInputBox();
     if (name) {
       workspaceUri = workspace.workspaceFolders[0].uri;
-      const codioWorkspaceFolderPath = await createWorkspaceCodiosFolder(workspaceUri);
-      codioUri = Uri.file(join(codioWorkspaceFolderPath, `${name.split(' ').join('_')}.codio`));
+      codioUri = await getAvailableUri(name, workspaceUri);
+      name = basename(codioUri.path, '.codio');
       getCodioName = async () => name;
     }
   } else {
@@ -29,6 +30,37 @@ export const getWorkspaceUriAndCodioDestinationUri = async () => {
 
   return { workspaceUri, codioUri, getCodioName };
 };
+
+/**
+ * Get a URI that does not exist and safe to write to.
+ * @param name Name to save codio to.
+ * @param workspaceUri Workspace folder to save codio to.
+ * @returns A safe URI to write the new given codio to.
+ */
+const getAvailableUri = async (name: string, workspaceUri: Uri): Promise<vscode.Uri> => {
+  let uri: Uri;
+  let append = '';
+  let count = 0;
+  let filename;
+  let fileStat: vscode.FileStat;
+  const codioWorkspaceFolderPath = await createWorkspaceCodiosFolder(workspaceUri);
+
+  do {
+    filename = `${name.split(' ').join('_')}${append}.codio`;
+    uri = Uri.file(join(codioWorkspaceFolderPath, filename));
+    try {
+      fileStat = await vscode.workspace.fs.stat(uri);
+      if (fileStat.type === vscode.FileType.File) {
+        count++;
+        append = `_${count.toString().padStart(2, '0')}`;
+      }
+    } catch (e) { // File doesn't exist and available to write to.
+      fileStat = null;
+    }
+  } while (fileStat);
+
+  return uri;
+}
 
 export const getWorkspaceRootAndCodiosFolder = ():
   | { workspaceRootUri: Uri; workspaceCodiosFolder: string }
